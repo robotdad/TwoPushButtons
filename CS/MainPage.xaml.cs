3bae1327e2accfc2578b8a2a97db3c90849986de
Marc Goodner <mrgoodner@hotmail.com>
2015-05-02 16:33:08 -0700

    First commit, working to blink each led
    what it says

---
+++ CS/MainPage.xaml.cs
@@ -0,0 +1,149 @@
+﻿/*
+    Copyright(c) Microsoft Open Technologies, Inc. All rights reserved.
+
+    The MIT License(MIT)
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files(the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions :
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+*/
+
+using System;
+using Windows.Devices.Gpio;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Media;
+
+namespace PushButton
+{
+    public sealed partial class MainPage : Page
+    {
+        private GpioPinValue pushButtonValue;
+        public MainPage()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(500);
+            timer.Tick += Timer_Tick;
+            timer.Start();
+
+            Unloaded += MainPage_Unloaded;
+
+            InitGPIO();
+        }
+
+        private void InitGPIO()
+        {
+            var gpio = GpioController.GetDefault();
+
+            // Show an error if there is no GPIO controller
+            if (gpio == null)
+            {
+                nopin = null;
+                GpioStatus.Text = "There is no GPIO controller on this device.";
+                return;
+            }
+            nopushButton = gpio.OpenPin(NOPB_PIN);
+            nopin = gpio.OpenPin(NO_PIN);
+            yespushButton = gpio.OpenPin(YESPB_PIN);
+            yespin = gpio.OpenPin(YES_PIN);
+
+            // Show an error if the pin wasn't initialized properly
+            if (nopin == null)
+            {
+                GpioStatus.Text = "There were problems initializing the GPIO No LED pin.";
+                return;
+            }
+            if (nopushButton == null)
+            {
+                GpioStatus.Text = "There were problems initializing the GPIO No Push Button pin.";
+                return;
+            }
+            if (yespin == null)
+            {
+                GpioStatus.Text = "There were problems initializing the GPIO Yes LED pin.";
+                return;
+            }
+            if (yespushButton == null)
+            {
+                GpioStatus.Text = "There were problems initializing the GPIO Yes Push Button pin.";
+                return;
+            }
+
+            nopushButton.SetDriveMode(GpioPinDriveMode.Input);
+            nopin.SetDriveMode(GpioPinDriveMode.Output);
+            yespushButton.SetDriveMode(GpioPinDriveMode.Input);
+            yespin.SetDriveMode(GpioPinDriveMode.Output);
+
+            GpioStatus.Text = "GPIO pins initialized correctly.";
+        }
+
+        private void MainPage_Unloaded(object sender, object args)
+        {
+            // Cleanup
+            nopin.Dispose();
+            nopushButton.Dispose();
+            yespin.Dispose();
+            yespushButton.Dispose();
+        }
+
+        private void FlipLED(GpioPin pb, GpioPin pin, SolidColorBrush brush, string msg)
+        {
+            pushButtonValue = pb.Read();
+            if (pushButtonValue == GpioPinValue.Low)
+            {
+                LED.Fill = brush;
+                GpioStatus.Text = msg;
+                pin.Write(GpioPinValue.Low);
+            }
+            else if (pushButtonValue == GpioPinValue.High)
+            {
+                LED.Fill = grayBrush;
+                GpioStatus.Text = "I'm waiting...";
+                pin.Write(GpioPinValue.High);
+            }
+        }
+
+       
+
+       private void Timer_Tick(object sender, object e)
+        {
+            FlipLED(nopushButton, nopin, redBrush, "boo!");
+            FlipLED(yespushButton, yespin, greenBrush, "yay!");
+        }
+
+       
+        /// <summary>
+        /// 
+        /// </summary>
+        private const int NO_PIN = 27;
+        private const int NOPB_PIN = 5;
+        private GpioPin nopin;
+        private GpioPin nopushButton;
+        private const int YES_PIN = 22;
+        private const int YESPB_PIN = 6;
+        private GpioPin yespin;
+        private GpioPin yespushButton;
+        private DispatcherTimer timer;
+        private SolidColorBrush redBrush = new SolidColorBrush(Windows.UI.Colors.Red);
+        private SolidColorBrush greenBrush = new SolidColorBrush(Windows.UI.Colors.Green);
+        private SolidColorBrush grayBrush = new SolidColorBrush(Windows.UI.Colors.LightGray);
+
+    }
+}
