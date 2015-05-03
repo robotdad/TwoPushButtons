8986caa0feb8a2af1609a8346519bf026b23e2b4
Marc Goodner <mrgoodner@hotmail.com>
2015-05-02 20:39:43 -0700

    Adding some output messages
    like it says

--- Python/TwoPushButtons.py
+++ Python/TwoPushButtons.py
@@ -10,17 +10,20 @@ GPIO.setup(6, GPIO.IN)
 GPIO.setup(27, GPIO.OUT)
 GPIO.setup(22, GPIO.OUT)
 
+print "I'm waiting..."
 try:
 	while True:
 
 		# led button 1 is pressed
 		if ( GPIO.input(5) == False ):
 			GPIO.output(27, GPIO.LOW)
+			print("Boo!")
 		else:
 			GPIO.output(27, GPIO.HIGH)
 		# led button 2 is pressed
 		if ( GPIO.input(6) == False ):
 			GPIO.output(22, GPIO.LOW)
+			print("Yay!")
 		else:
 			GPIO.output(22, GPIO.HIGH)
 
