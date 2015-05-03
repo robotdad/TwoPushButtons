357e0ed5e06b7356bb91cd0f8a2b972f752836fa
Marc Goodner <mrgoodner@hotmail.com>
2015-05-02 20:34:33 -0700

    Adding project files
    and fixing some egregiously stupid mistakes

--- Python/TwoPushButtons.py
+++ Python/TwoPushButtons.py
@@ -1,27 +1,28 @@
 from time import sleep
 import RPi.GPIO as GPIO
 import ptvsd
+ptvsd.enable_attach(secret=None)
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(5, GPIO.OUT)
-GPIO.setup(6, GPIO.OUT)
-GPIO.setup(27, GPIO.IN)
-GPIO.setup(22, GPIO.IN)
+GPIO.setup(5, GPIO.IN)
+GPIO.setup(6, GPIO.IN)
+GPIO.setup(27, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 try:
 	while True:
 
-	    # led button 1 is pressed
-		if ( GPIO.input(27) == False ):
-			GPIO.output(5, GPIO.LOW)
-		else
-			GPIO.output(5, GPIO.HIGH)
+		# led button 1 is pressed
+		if ( GPIO.input(5) == False ):
+			GPIO.output(27, GPIO.LOW)
+		else:
+			GPIO.output(27, GPIO.HIGH)
 		# led button 2 is pressed
-		if ( GPIO.input(22) == False ):
-			GPIO.output(6, GPIO.LOW)
-		else
-			GPIO.output(6, GPIO.HIGH)
+		if ( GPIO.input(6) == False ):
+			GPIO.output(22, GPIO.LOW)
+		else:
+			GPIO.output(22, GPIO.HIGH)
 
 except KeyboardInterrupt:  
 	GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
