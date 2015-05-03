487fdd76b2049d6ba6223ddfb78440fb6893708a
Marc Goodner <mrgoodner@hotmail.com>
2015-05-02 19:18:55 -0700

    First cut at a python version
    what it says

---
+++ Python/TwoPushButtons.py
@@ -0,0 +1,30 @@
+from time import sleep
+import RPi.GPIO as GPIO
+import ptvsd
+
+GPIO.setmode(GPIO.BCM)
+
+GPIO.setup(5, GPIO.OUT)
+GPIO.setup(6, GPIO.OUT)
+GPIO.setup(27, GPIO.IN)
+GPIO.setup(22, GPIO.IN)
+
+try:
+    while True:
+
+	    # led button 1 is pressed
+        if ( GPIO.input(27) == False ):
+			GPIO.output(5, GPIO.LOW)
+		else
+			GPIO.output(5, GPIO.HIGH)
+		# led button 2 is pressed
+		if ( GPIO.input(22) == False ):
+			GPIO.output(6, GPIO.LOW)
+		else
+			GPIO.output(6, GPIO.HIGH)
+
+except KeyboardInterrupt:  
+    GPIO.cleanup()       # clean up GPIO on CTRL+C exit  
+
+GPIO.cleanup()      
+
